c697c315b263edebd95d9c993a0b21bc7cc25941
Kholofelo <rkmoropane@gmail.com>
2026-06-17 12:14:28 -0400

    Fixed the question 11.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -65,6 +65,25 @@ public_users.get('/',async function (req, res) {
     // return res.send(JSON.stringify(books, null, 4));
 });
 
+// Get all books using Async/Await + Axios
+public_users.get('/async/books', async (req, res) => {
+    try {
+
+        const response = await axios.get(
+            'http://localhost:5000/'
+        );
+
+        return res.send(response.data);
+
+    } catch (error) {
+
+        return res.status(500).json({
+            message: error.message
+        });
+
+    }
+});
+
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', async function (req, res) {
     //Write your code here
@@ -99,6 +118,29 @@ public_users.get('/isbn/:isbn', async function (req, res) {
     
     }
  });
+
+//  Get book by ISBN using Axios
+ public_users.get('/async/isbn/:isbn', async (req, res) => {
+
+    try {
+
+        const isbn = req.params.isbn;
+
+        const response = await axios.get(
+            `http://localhost:5000/isbn/${isbn}`
+        );
+
+        return res.send(response.data);
+
+    } catch (error) {
+
+        return res.status(404).json({
+            message: error.message
+        });
+
+    }
+
+});
   
 // Get book details based on author
 public_users.get('/author/:author', async function (req, res) {
@@ -137,6 +179,29 @@ public_users.get('/author/:author', async function (req, res) {
     }
 });
 
+// Get books by author using Axios
+public_users.get('/async/author/:author', async (req, res) => {
+
+    try {
+
+        const author = req.params.author;
+
+        const response = await axios.get(
+            `http://localhost:5000/author/${author}`
+        );
+
+        return res.send(response.data);
+
+    } catch (error) {
+
+        return res.status(404).json({
+            message: error.message
+        });
+
+    }
+
+});
+
 // Get all books based on title
 public_users.get('/title/:title', async function (req, res) {
     //Write your code here
@@ -173,6 +238,29 @@ public_users.get('/title/:title', async function (req, res) {
             message: 'Error retrieving books'
         });
     }
+});
+
+// Get books by title using Axios
+public_users.get('/async/title/:title', async (req, res) => {
+
+    try {
+
+        const title = req.params.title;
+
+        const response = await axios.get(
+            `http://localhost:5000/title/${title}`
+        );
+
+        return res.send(response.data);
+
+    } catch (error) {
+
+        return res.status(404).json({
+            message: error.message
+        });
+
+    }
+
 });
 
 //  Get book review
